b9b8d7e719d7b428a44007dd5cff2c86d8a7dffb
Carlos Martin <s_martincj@globalhitss.com>
2023-03-22 17:53:21 -0600

    se usa env se crean las primeras rutas se agrega archivo example se cambian los puertos en la imagen

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module pajaro.com/curso-go
 
 go 1.18
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"pajaro.com/curso-go/sistema"
+)
+
 
 func main() {
-	fmt.Println("Hello World")
+	// cargamos puerto del archivo .env
+	port := sistema.GoDotEnvVariable("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+	// router
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hola Mundo")
+	})
+	fmt.Println("El servidor esta corriendo en el puerto 3000")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
