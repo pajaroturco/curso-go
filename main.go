faaf34142344035095cfa6a3f5fc67fc78ff55ce
Carlos Martin <s_martincj@globalhitss.com>
2023-03-22 18:04:18 -0600

    se agrega funcion de pagina 404

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"pajaro.com/curso-go/sistema"
 )
 
+func paginaNF(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "Pagina no encontrada")
+	http.NotFound(w, r)
+}
 
 func main() {
 	// cargamos puerto del archivo .env
@@ -19,6 +23,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hola Mundo")
 	})
+	http.HandleFunc("/page", paginaNF)
 	fmt.Println("El servidor esta corriendo en el puerto " + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
